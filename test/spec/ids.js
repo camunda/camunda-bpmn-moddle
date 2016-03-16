abf01245a59c72b3d4cff1df97de8d6745f62dd6
Nico Rehwaldt <git_nikku@nixis.de>
2016-03-16 16:05:38 +0100

    test(moddle): verify correct camunda:FormField id handling
    Related to bpmn-io/bpmn-js#493

---
+++ test/spec/ids.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var readFile = require('../helper').readFile,
+    createModdle = require('../helper').createModdle;
+
+
+describe('ids', function() {
+
+  var moddle;
+
+  beforeEach(function() {
+    moddle = createModdle();
+  });
+
+
+  describe('id validation', function() {
+
+    it('should ignore id property on camunda:FormField', function(done) {
+
+      var xml = readFile('test/fixtures/xml/camunda-formField-ids.bpmn');
+
+      var moddle = createModdle();
+
+      // when
+      moddle.fromXML(xml, 'bpmn:Definitions', function(err, definitions, context) {
+
+        if (err) {
+          return done(err);
+        }
+
+        var elementsById = context.elementsById;
+
+        // then
+        expect(context.warnings).to.be.empty;
+
+        expect(elementsById).not.to.have.property('variableA');
+
+        done();
+      });
+
+    });
+
+  });
+
+});
