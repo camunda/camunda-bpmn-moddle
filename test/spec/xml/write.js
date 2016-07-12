08599e18473395ef3a5c96f8dabcbfa9c78682c4
Kristin Polenz <kristin.polenz@camunda.com>
2016-07-12 11:39:29 +0200

    feat(field): change expression from attr to element
    - add lodash dependency
    - add write tests for camunda:field element
    
    related to #172

--- test/spec/xml/write.js
+++ test/spec/xml/write.js
@@ -1,5 +1,102 @@
 'use strict';
 
+var assign = require('lodash/object/assign'),
+    isFunction = require('lodash/lang/isFunction');
+
+var Helper = require('../../helper');
+
+
 describe('write', function() {
+
+  var moddle = Helper.createModdle();
+
+
+  function write(element, options, callback) {
+    if (isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    // skip preamble for tests
+    options = assign({ preamble: false }, options);
+
+    moddle.toXML(element, options, callback);
+  }
+
+
+  describe('should export types', function() {
+
+    it('Field#stringValue', function(done) {
+
+      // given
+      var fieldElem = moddle.create('camunda:Field', {
+        name: 'Field_1',
+        stringValue: 'myFieldValue'
+      });
+
+      var expectedXML =
+        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" ' +
+              'name="Field_1" stringValue="myFieldValue" />';
+
+      // when
+      write(fieldElem, function(err, result) {
+
+        // then
+        expect(result).to.eql(expectedXML);
+
+        done(err);
+      });
+    });
+
+
+    it('Field#string', function(done) {
+
+      // given
+      var fieldElem = moddle.create('camunda:Field', {
+        name: 'Field_1',
+        string: 'myStringValue'
+      });
+
+      var expectedXML =
+        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" name="Field_1">' +
+          '<camunda:string>myStringValue</camunda:string>' +
+        '</camunda:field>';
+
+      // when
+      write(fieldElem, function(err, result) {
+
+        // then
+        expect(result).to.eql(expectedXML);
+
+        done(err);
+      });
+    });
+
+
+    it('Field#expression', function(done) {
+
+      // given
+      var fieldElem = moddle.create('camunda:Field', {
+        name: 'Field_1',
+        expression: '${myExpressionValue}'
+      });
+
+      var expectedXML =
+        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" name="Field_1">' +
+          '<camunda:expression>${myExpressionValue}</camunda:expression>' +
+        '</camunda:field>';
+
+      // when
+      write(fieldElem, function(err, result) {
+
+        // then
+        expect(result).to.eql(expectedXML);
+
+        done(err);
+      });
+    });
+
+
+  });
 
 });
