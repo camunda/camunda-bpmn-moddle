1599e5e4af800dca7fcf2d2cec1105a277fe264c
David Carvalho <david.carvalho@wit-software.com>
2017-05-26 12:25:04 +0100

    ES6 importing of Camunda Moddle Extension

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// this module does not expose any public API (yet!)
+/*
+	Import and using it with ES6:
+	----------------------------
+	import * as BpmnModeler from 'bpmn-js/lib/Modeler';
+	import { CamundaModdleExtension, CamundaModdleExtensionJson } from 'camunda-bpmn-moddle';
+
+
+	Using it:
+	---------
+	const modeler = new BpmnModeler({
+		additionalModules: [
+			CamundaModdleExtension
+		],
+		moddleExtensions: {
+			camunda: CamundaModdleExtensionJson
+		}
+	});
+
+*/
+
+'use strict';
+
+module.exports = {
+  __init__: [ 'CamundaModdleExtension' ],
+  CamundaModdleExtension: [ 'type', require('./lib/extension') ],
+  CamundaModdleExtensionJson: require('./resources/camunda.json')
+};
